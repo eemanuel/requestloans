1b3c91a2fe216e711eb76bb72d376668b6e03410
eemanuel <eemanueloviedo@gmail.com>
2020-02-22 18:47:34 -0300

    Update loans.views .

--- requestloans/loans/views.py
+++ requestloans/loans/views.py
@@ -13,8 +13,9 @@ from .requester import LoanRequester
 
 class LoanCreateView(CreateView):
     form_class = LoanForm
-    template_name = ''
+    template_name = 'loans/loan_create.html'
     success_url = reverse_lazy('loan-create')
+    success_message = "Loan message"
 
     def form_valid(self, form):
         data = {
@@ -25,11 +26,34 @@ class LoanCreateView(CreateView):
         loan_requester = LoanRequester()
         loan_response = loan_requester.request(data)
         loan_form = LoanRequesterForm(data=loan_response.json())
-        import ipdb; ipdb.set_trace()
         if not loan_form.is_valid():
             self.success_message = "Loan Reject"
             return HttpResponseRedirect(self.get_success_url())
         self.success_message = "Loan Accepted"
         form.instance.approved = True
         return super().form_valid(form)
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class LoanListView(ListView):
+    model = Loan
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class LoanDetailView(DetailView):
+    model = Loan
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class LoanUpdateView(UpdateView):
+    model = Loan
+    form_class = LoanForm
+    template_name = 'loans/loan_create.html'
+    success_url = reverse_lazy('loan-list')
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class LoanDeleteView(DeleteView):
+    model = Loan
+    success_url = reverse_lazy('loan-list')
 
