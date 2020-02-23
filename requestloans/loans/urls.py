f155313c11a1e1abe38c7ecdc9062a71a8c8648f
eemanuel <eemanueloviedo@gmail.com>
2020-02-22 21:47:04 -0300

    Update loans.urls .

--- requestloans/loans/urls.py
+++ requestloans/loans/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     url(r'^$', LoanCreateView.as_view(), name='loan-create'),
     url(r'^list/', LoanListView.as_view(), name='loan-list'),
-    url(r'^detail/(?P<pk>\d+)/', LoanDetailView.as_view(), name='loan-detail'),
-    url(r'^update/(?P<pk>\d+)/', LoanUpdateView.as_view(), name='loan-update'),
-    url(r'^delete/(?P<pk>\d+)/', LoanDeleteView.as_view(), name='loan-delete'),
+    url(r'^(?P<pk>\d+)detail//', LoanDetailView.as_view(), name='loan-detail'),
+    url(r'^(?P<pk>\d+)/update/', LoanUpdateView.as_view(), name='loan-update'),
+    url(r'^(?P<pk>\d+)/delete/', LoanDeleteView.as_view(), name='loan-delete'),
 ]
